aac9727c0d4f30fa8917ba5a8f72625e653239ca
Oscar Barba <racso_181088@hotmail.com>
2026-08-16 17:08:04 -0600

    chore: scaffold Next.js + TypeScript project

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function HomePage() {
+  return (
+    <main>
+      <h1>Open Party</h1>
+      <p>My Events will appear here once you open an invitation link.</p>
+    </main>
+  )
+}
